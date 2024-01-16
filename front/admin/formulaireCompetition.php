67848fc3578f475f829cd2e294751763a8783a4b
sabine-thb <sabine.thibout@edu.univ-eiffel.fr>
2024-01-16 17:44:49 +0100

    frontAdmin

--- front/admin/formulaireCompetition.php
+++ front/admin/formulaireCompetition.php
@@ -1,20 +1,23 @@
-<div class = "container d-flex justify-content-center" >
-    <form action="admin.php?action=ajoutCompetition" method="post" class="align-items-center justify-content-center justify-content-md-between">
-        
-        <label>
-            nom de la competition :
-            <input class='form-control mr-sm-2' type='text' name='name'>
-        </label>
+<section class="creerCompet">
+    <h1 class="titlePage">Créer une nouvelle compétition</h1>
+    <form action="admin.php?action=ajoutCompetition" method="post" class="formCompet">
+        <p class="nomCompet">
+            <label for="nom" class="labelCompet">Nom de la compétition : </label>
+            <input class='' type='text' name='name' id="nom">
 
-        <label>
-            Courte description :
-            <textarea class="form-control mr-sm-2" name="description" rows="4" cols="50"></textarea>
-        </label>
+        </p>
+        
+        <p class="descrCompet">
+            <label for="descr" class="labelCompet">Courte description : </label>
+            <textarea class="areaDescr" name="description" id="descr"></textarea>
+
+        </p>
+        
 
         <p>
             <?php echo $erreur; ?>
         </p>
 
-        <input class="btn btn-outline-success my-2 my-sm-0" type="submit" value="créer">
+        <input class="creer" type="submit" value="créer">
     </form>
-</div>
+</section>
--- front/admin/formulaireCreationEquipe.php
+++ front/admin/formulaireCreationEquipe.php
@@ -1,22 +1,23 @@
-<div >
-
+<section class="creaEquipe">
+        <h1 class="titlePage">Je créé une nouvelle équipe : </h1>
         <form action="admin.php?action=ajoutEquipe" method="post"
-              enctype="multipart/form-data" class="align-items-center justify-content-center justify-content-md-between">
-                
-            <label>
-                Nom de la Team:
-                <input class='form-control mr-sm-2' type='text' name='name' required/>
-            </label><br>
+              enctype="multipart/form-data" class="formEquipe">
+             <p class="p equipe">
+                 <label for="nomEquipe" class="labelEquipe">Nom de la Team : </label>
+                 <input class='form-control mr-sm-2' type='text' name='name' id="nomEquipe"required/>
+             </p> 
+             <p class="p equipe">
+                 <label for="logo" class="labelEquipe">Logo (max 2 Mo) :</label>
+                 <input type="hidden" name="MAX_FILE_SIZE" value="2000000" id="logo">
+                 <input type="file" name="logo" accept="image/*"/>
 
-            <label>
-                Logo (max 2 Mo):
-                <input type="hidden" name="MAX_FILE_SIZE" value="2000000">
-                <input type="file" name="logo" accept="image/*"/>
-            </label>
+             </p>  
+            
 
+            
 
             <input type="hidden" name="token" value="<?php echo $token; ?>"/>
 
-            <input class="btn btn-outline-success my-2 my-sm-0" type="submit" value="créer"/>
+            <input class="creerEquipe" type="submit" value="créer"/>
         </form>
-</div>
+</section>
