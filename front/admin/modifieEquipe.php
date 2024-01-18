6f16effb4621623a2a83971bf7e482e900dcd787
sabine-thb <sabine.thibout@edu.univ-eiffel.fr>
2024-01-18 01:15:40 +0100

    pageGererEquipe

--- front/admin/modifieEquipe.php
+++ front/admin/modifieEquipe.php
@@ -1,20 +1,21 @@
-<form action="admin.php?action=modifieEquipe" method="post"
-      enctype="multipart/form-data">
+<section>
+    <form action="admin.php?action=modifieEquipe" method="post"
+        enctype="multipart/form-data">
 
-    <label>
-        Nom de la Team:
-        <input type='text' name='name' value="<?php echo $equipe['nom']; ?>" required/>
-    </label><br>
+        <label for="nomEquipe">Nom de la Team:</label>
+            <input type='text' name='name' id="nomEquipe" value="<?php echo $equipe['nom']; ?>" required/>
+        
 
-    <img src="<?php echo $equipe['srcLogo']; ?>" alt="logo de l'equipe">
+        <img src="<?php echo $equipe['srcLogo']; ?>" alt="logo de l'equipe">
 
-    <label>
-        Logo (max 2 Mo):
-        <input type="hidden" name="MAX_FILE_SIZE" value="2000000">
-        <input type="file" name="logo" accept="image/*"/>
-    </label>
+        <label for="logo">Logo (max 2 Mo):</label>
+            <input type="hidden" name="MAX_FILE_SIZE" value="2000000" id="logo">
+            <input type="file" name="logo" accept="image/*"/>
+        
 
 
-    <input type="hidden" name="idEquipe" value="<?php echo $equipe['equipe_id']; ?>"/>
-    <input type="submit" value="modifier"/>
-</form>
+        <input type="hidden" name="idEquipe" value="<?php echo $equipe['equipe_id']; ?>"/>
+        <input type="submit" value="modifier"/>
+    </form>
+
+</section>
