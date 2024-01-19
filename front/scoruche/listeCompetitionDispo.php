d4ab69071dc8abb177f36a506819b9d3e5a52582
sabine-thb <sabine.thibout@edu.univ-eiffel.fr>
2024-01-19 10:40:16 +0100

    modifs

--- front/scoruche/listeCompetitionDispo.php
+++ front/scoruche/listeCompetitionDispo.php
@@ -1,16 +1,13 @@
 
 <section>
-
     <?php if(!empty($tableau)){ ?>
         <p class="titleCompet">
             Voici les compétitions disponibles :
         </p>
-        <div>
             <?php foreach ($tableau as $tuple) { ?>
-                <div>
-                    <div>
+                <div class="oneCompet">
 
-                        <h5>
+                    <h5>
                             <?php echo $tuple["nom"] . '  -  ' . $tuple["date_creation"]; ?>
                         </h5>
                         <p>
@@ -21,12 +18,11 @@
                             <i class="fa-solid fa-door-open"></i>
                         </a>
 
-                    </div>
                 </div>
+
             <?php } ?>
-        </div>
     <?php }else{ ?>
-        <p>Il n'y a aucune compétition disponible pour le moment</p>
+        <p>Il n'y a aucune compétition disponible pour le moment.</p>
         <img src="./style/gif/pageVideHomer.gif" width="320" height="240" frameBorder="0" alt="gif de homer"/>
     <?php } ?>
 </section>
