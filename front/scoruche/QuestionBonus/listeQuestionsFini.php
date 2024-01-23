e7c5c467ee2df529a529efca45d001bd603bcf82
sabine-thb <sabine.thibout@edu.univ-eiffel.fr>
2024-01-23 12:25:33 +0100

    frontBonusPart2

--- front/scoruche/QuestionBonus/listeQuestionsFini.php
+++ front/scoruche/QuestionBonus/listeQuestionsFini.php
@@ -1,5 +1,10 @@
 <section>
-    <a href="competition.php?action=questionsBonus&id=<?php echo $_GET['id']; ?>&type=attente" class="lienBonus">
-        Bonus en attente
-    </a>
+    <h1 class="titlePage">Voici les bonus qui ont expirés :</h1>
+    <div class="bonusContainer">
+        <a href="competition.php?action=questionsBonus&id=<?php echo $_GET['id']; ?>&type=attente" class="lienBonus">
+            Voir les bonus en attente
+        </a>
+
+    </div>
+    
 </section>
