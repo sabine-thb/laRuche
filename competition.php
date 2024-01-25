286552f799e8c5413a54676b24f81df18c61c70c
amaitre <manbrozer@gmail.com>
2024-01-25 20:09:12 +0100

    fix : fleche prono match

--- competition.php
+++ competition.php
@@ -38,6 +38,7 @@ $affichageModule = $module->afficheModule();
     <title>LaRuche - scoruche</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/239660ff21.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style/css/compet.css">
     <link rel="stylesheet" href="style/css/inputScore.css">
     <link rel="stylesheet" href="style/css/menu.css">
