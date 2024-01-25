89e49ec70b0a8f69baa703dbe9f0031ad21bc67d
amaitre <manbrozer@gmail.com>
2024-01-25 14:33:07 +0100

    fix : bug nom table pour la version dev

--- profil.php
+++ profil.php
@@ -27,7 +27,6 @@ $affichageModule = $module->afficheModule();
     <title>LaRuche - scoruche</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://kit.fontawesome.com/239660ff21.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style/css/menu.css">
     <link rel="stylesheet" href="style/css/fonts.css">
     <link rel="stylesheet" href="style/css/profil.css">
