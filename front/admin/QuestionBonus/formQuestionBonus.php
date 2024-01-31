42400eaae41f4eff072c5d371f567902e47c6574
sabine-thb <sabine.thibout@edu.univ-eiffel.fr>
2024-01-31 15:03:55 +0100

    reponsiveBonus

--- front/admin/QuestionBonus/formQuestionBonus.php
+++ front/admin/QuestionBonus/formQuestionBonus.php
@@ -1,13 +1,13 @@
 <section class="creerCompet">
-    <h1 class="titlePage">Créer une question Bonus</h1>
+    <h1 class="titlePage">Créer une question Bonus :</h1>
     <form action="admin.php?action=ajouteQuestionBonus" method="post" class="formCompet">
         <p class="nomCompet">
             <label for="nom" class="labelCompet">Titre de la question : </label>
             <input class='' type='text' name='name' id="nom" maxlength="99" required>
         </p>
 
-        <p>
-            <label for="selectCompet" class="labelCompet">competition :</label>
+        <p class="nomCompet">
+            <label for="selectCompet" class="labelCompet">Compétition :</label>
                 <select name="compet" id="selectCompet" required>
                     <nom>Compétition</nom>
                     <?php foreach($comp as $row){
@@ -21,16 +21,21 @@
             <textarea class="areaDescr" name="description" id="descr" maxlength="149"></textarea>
         </p>
 
-        <label for="typeResultat" class="labelCompet">Type resultat :</label>
-        <select name="typeResultat" id="typeResultat" required>
-            <option value="nombre">nombre</option>
-            <option value="string">nom joueur</option>
-            <option value="equipe">equipe</option>
-            <option value="bool">reponse fermé (oui/non)</option>
-        </select>
+        <p class="nomCompet">
+            <label for="typeResultat" class="labelCompet">Type résultat :</label>
+            <select name="typeResultat" id="typeResultat" required>
+                <option value="nombre">nombre</option>
+                <option value="string">nom joueur</option>
+                <option value="equipe">equipe</option>
+                <option value="bool">reponse fermé (oui/non)</option>
+            </select>
 
-        <p>
-            <label for="nbPoint" class="labelCompet">Point bonne reponse : </label>
+        </p>
+
+        
+
+        <p class="nomCompet">
+            <label for="nbPoint" class="labelCompet">Points bonne réponse : </label>
             <input type="number" inputmode="numeric" min="0" max="9999" name="points" id="nbPoint" required>
         </p>
 
