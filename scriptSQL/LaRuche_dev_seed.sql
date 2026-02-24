b518ffbb0fe52b2177f54f1cfbc0cc16a2c2058c
amaitre <manbrozer@gmail.com>
2026-02-24 10:22:39 +0100

    ajout de compte admin par defaut pour l'env de dev

---
+++ scriptSQL/LaRuche_dev_seed.sql
@@ -0,0 +1,4 @@
+-- Seed de developpement uniquement - NE PAS utiliser en production
+-- Compte admin par defaut (login: admin / password: admin)
+INSERT INTO LaRuche_admin (login, password)
+VALUES ('admin', '$2y$12$newH7mzxcDV7jmICoXQgB.QvFRml1fDOOMFGEdeSMjizfzoHdvJlC');
